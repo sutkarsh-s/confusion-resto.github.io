bd123dea3835f55f4c884877b2716e20af4dd590
sutkarsh-s <64131396+sutkarsh-s@users.noreply.github.com>
2021-07-16 03:14:02 +0530

    restaurant website

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyAapQpUliM2YipDR9ANFfpfXrtQFLLd948",
+  authDomain: "confusion-server-29eea.firebaseapp.com",
+  projectId: "confusion-server-29eea",
+  storageBucket: "confusion-server-29eea.appspot.com",
+  messagingSenderId: "275419489555",
+  appId: "1:275419489555:web:90b3be849481412d7be6e6",
+  measurementId: "G-WPLCN0BCG7"
+};
